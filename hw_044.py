2de4203a731e71a4ba54bd45b52a606eaddb9b3a
Auphie <auphie@gmail.com>
2020-12-14 21:25:57 +0800

    1 of hw_44

--- hw_044.py
+++ hw_044.py
@@ -1,10 +1,15 @@
+import json
+
 def input_string():
     str_lists = []
     a = input()
-    for i in range(int(a)):
-        inpStr = input()
-        str_lists.append(inpStr)
-    return str_lists
+    if int(a)==0:
+        return('')
+    else:
+        for i in range(int(a)):
+            inpStr = input()
+            str_lists.append(inpStr)
+        return str_lists
 
 def split_input(b):
     splitted=[]
@@ -15,9 +20,9 @@ def split_input(b):
 
 def get_seperate_point(splitted):
     sep_point=[2]
-    for i in splitted[3:]:
-        if i.isnumeric():
-            sep_point.append(splitted.index(i))
+    for i in range(3,len(splitted)):
+        if splitted[i].isnumeric():
+            sep_point.append(i)
     return sep_point
 
 def make_info(splitted, sep):
@@ -30,16 +35,23 @@ def make_info(splitted, sep):
     info['items']=item
     return info
 
-b='breakfast 7 11 breakfast burritos 60 pancakes 40'
+b='breakfast 7 11 breakfast burritos 60 pancakes 60'
 c='lunch 11 3 hamburger 50'
 d='dinner 3 10 spaghetti 80'
 
-manu={}
-#str_lists = input_string()
-for i in (b,c,d):
+menu={}
+str_lists = input_string()
+for i in str_lists:
+#for i in (b,c,d):
     splitted=split_input(i)
     sep = get_seperate_point(splitted)
     info = make_info(splitted, sep)
-    manu[splitted[0]]=info
+    menu[splitted[0]]=info
 
-print(manu)
+
+aa=json.dumps(menu,indent=2)
+bb = aa.replace('\"','\'')
+if len(menu)==0:
+    print('menu = '+'{\n}')
+else:
+    print('menu = '+bb)
