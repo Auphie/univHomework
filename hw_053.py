0c27162c072aa610257471dc003979bbeb68a193
Auphie <auphie@gmail.com>
2021-01-06 22:46:50 +0800

    HW53 firm

--- hw_053.py
+++ hw_053.py
@@ -8,26 +8,31 @@ def get_input(board):
     return board
 
 def valid_line(lines, player):
-    for i in range(len(lines)-5):
-        if i < (len(lines)-4) and ((lines[i:i+5]==player).sum()==5 and (lines[i+5]!=player)):
+    if len(lines) < 5:
+        return False
+    for i in range(len(lines)-4):
+        if i < (len(lines)-5) and ((lines[i:i+5]==player).sum()==5 and (lines[i+5]!=player)):
             return True
-        if (i == (len(lines)-4)) and ((row[i:i+5]==player).sum()==5):
+        if (i == (len(lines)-5)) and ((lines[i:i+5]==player).sum()==5):
             return True
     return False
 
 def checkWinner(data, player):
-    result = False
-    diagonalL = data.diagonal()
-    diagonalR = np.fliplr(data).diagonal()
+    diagonalL = [np.diagonal(data, offset=i) for i in range(3-len(data),len(data)-3)]
+    diagonalR = [np.fliplr(data).diagonal(offset=i)for i in range(3-len(data),len(data)-3)]
     for row in data:
-        result1 =  valid_line(row, player)
+        if valid_line(row, player):
+            return True
     for col in data.T:
-        result2 =  valid_line(col, player)
-    result3 = valid_line(diagonalL, player)
-    result4 = valid_line(diagonalR, player)
-    print('res1=%s, res2=%s, res3=%s, res4=%s'%(result1, result2, result3, result4))
-    result = result or result2 or result3 or result4
-    return result
+        if valid_line(col, player):
+            return True
+    for backslash in diagonalL:
+        if valid_line(backslash, player):
+            return True
+    for slash in diagonalR:
+        if valid_line(slash, player):
+            return True
+    return False
 
 def winMove(data, player):
     tempBoard = copy.deepcopy(data)
@@ -52,25 +57,13 @@ def check_result(data):
             result='{} win'.format(player)
     if (data==0).sum()==0:
         result = 'tie'
-#    elif is_finished == 0:
-#        x, y = winMove(data, nextPlayer)
-#        result = str(x)+str(y)
+    elif is_finished == 0:
+        x, y = winMove(data, nextPlayer)
+        result = str(x)+str(y)
     return result
 
-#board = np.zeros((10,10))
-#data = get_input(board)
-data = np.array([
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-[0, 2, 2, 2, 2, 2, 0, 0, 0, 0],
-[0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-[0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]])
-#result = check_result(data)
+board = np.zeros((10,10))
+data = get_input(board)
 
-result = checkWinner(data, 2)
+result = check_result(data)
 print(result)
