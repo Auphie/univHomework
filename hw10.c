9f405d9ede5888c3dd8ab2d63334dbbcd1fbb755
Auphie <auphie@gmail.com>
2021-03-14 17:00:17 +0800

    HW10

--- hw10.c
+++ hw10.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
-#include <math.h>
 
-void GrayCode(int n, int k){
-    if (n==1)
-        printf("%d\n",k);
-    else if (k < pow(2,(n-1))){
-        printf("%d",0);
-        return GrayCode(n-1, k);
+int simuCounts(int m){
+    int counts=0;
+    while (1){
+        if (m==1 || m==0)
+            break;
+        else if (m%2==0){
+            counts+=1;
+            m=m/2;
+        }
+        else {
+            counts+=1;
+            m=(m+1)/2;;
+        }
     }
-    else {
-        printf("%d",1);
-        return GrayCode(n-1, pow(2,n)-1-k);
+    return counts;
+}
+
+int bitToDecimal(char n[]){
+    int decimal;
+    for (int i=0; i<8;i++)
+        decimal=(decimal<<1)+n[i]-'0';
+    return decimal;
+}
+
+void decimalToBin(int n){
+    int k;
+    for (int i = 3; i >= 0; i--)  {
+        k = n >> i;
+        if (k & 1)
+        printf("1");
+        else
+        printf("0");
     }
+    printf("\n");
 }
 
 int main(void){
-    int n, k, next=0;
+    int decimal, counts, next=0;
+    char bitN[9];
     while (next != -1) {
-        scanf("%d %d", &n, &k);
-        GrayCode(n, k);
+        scanf("%s", bitN);
+        decimal = bitToDecimal(bitN);
+        counts = simuCounts(decimal);
+        decimalToBin(counts);
         scanf("%d", &next);
     }
+return 0;
 }
--- temp.c
+++ temp.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-#include <math.h>
 
-void GrayCode(int n, int k){
-    if (n==1)
-        printf("%d\n",k);
-    else if (k < pow(2,(n-1))){
-        printf("%d",0);
-        return GrayCode(n-1, k);
+int simuCounts(int m){
+    int counts=0;
+    while (1){
+        if (m==1 || m==0)
+            break;
+        else if (m%2==0){
+            counts+=1;
+            m=m/2;
+        }
+        else {
+            counts+=1;
+            m=(m+1)/2;
+        }
     }
-    else {
-        printf("%d",1);
-        return GrayCode(n-1, pow(2,n)-1-k);
+    return counts;
+}
+
+int bitToDecimal(char n[]){
+    int decimal;
+    for (int i=0; i<8;i++)
+        decimal=(decimal<<1)+n[i]-'0';
+    return decimal;
+}
+
+void decimalToBin(int n){
+    int k;
+    for (int i = 3; i >= 0; i--)  {
+        k = n >> i;
+        if (k & 1)
+        printf("1");
+        else
+        printf("0");
     }
+    printf("\n");
 }
 
 int main(void){
-    int n, k, next=0;
-    while (next != -1) {
-        scanf("%d %d", &n, &k);
-        GrayCode(n, k);
-        scanf("%d", &next);
-    };
+    int decimal, counts, Sum=0;
+    decimal = 125;
+    for (int i=decimal; i>=0; i--){
+        counts = simuCounts(i);
+        Sum+=counts;
+        printf("i=%d, c=%d\n", i, counts);
+    }
+printf("sum=%d",Sum);
+return 0;
 }
