bddb24f88397e7ea1e10c9d01e276d0584c7e397
Auphie <auphie@gmail.com>
2021-03-30 21:45:46 +0800

    hw12

--- hw12.c
+++ hw12.c
@@ -1,29 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void scan_fraction(int *numerator, int *denominator) {
-    char slash;
-    int status; /* status code returned by scanf indicating number of valid values obtained */
-    int error; 
-    char discard; 
-    do { 
-        error = 0;
-        printf("Enter a common fraction as two integers separated by a slash> ");
-        status = scanf("%d%c%d",&numerator, slash, &denominator);
-        if (status < 3) {
-            error = 1;
-            printf("Invalid-please read directions carefully\n");
+int gcd(int up, int down) {
+    if (down !=0)
+        return gcd(down,up%down);
+    else
+        return up; 
+}
+
+int lcm(int up,int down) {
+    return up/gcd(up,down)*down;
+}
+
+void reduction(int up,int down) {
+    int quotient = 0, GCD=gcd(up,down);
+    up/=GCD;
+    down/=GCD;
+    quotient = up/down;
+    if (up<0 || down <0)
+        printf("the result is -%d/%d\n",abs(up),abs(down));
+    else
+        printf("the result is %d/%d\n",up,down);
+}
+
+void add(int up1,int down1,int up2,int down2) {
+    int num1, num2, LCM=lcm(down1,down2), up, down;
+    num1=LCM/down1*up1;
+    num2=LCM/down2*up2;
+    up=num1+num2;
+    down=LCM;
+    reduction(up, down);
+}
+
+void sub(int up1,int down1,int up2,int down2) {
+    int num1, num2, LCM=lcm(down1,down2), up, down;
+    num1=LCM/down1*up1;
+    num2=LCM/down2*up2;
+    up=num1-num2;
+    down=LCM;
+    reduction(up,down);
+}
+
+void mul(int up1,int down1,int up2,int down2) {
+    int up, down;
+    up=up1*up2;
+    down=down1*down2;
+    reduction(up,down);
+}
+
+void div(int up1,int down1,int up2,int down2) {
+    int up, down;
+    up=up1*down2;
+    down=up2*down1;
+    reduction(up,down);
+}
+
+int main(void) {
+    int up1, up2, down1, down2;
+    char slash1, slash2, op, next='y';
+    while (next != 'n') {
+        scanf("%d%c%d", &up1, &slash1, &down1);
+        scanf("%d%c%d", &up2, &slash2, &down2);
+        scanf(" %c", &op);
+        scanf(" %c", &next);
+        if (slash1 != '/' && slash2 != '/') 
+            printf("ERROR\n");
+        else if (down1==0 || down2==0)
+            printf("ERROR\n");
+        else {
+            switch(op) {
+                case '+':add(up1,down1,up2,down2);break;
+                case '*':mul(up1,down1,up2,down2);break;
+                case '-':sub(up1,down1,up2,down2);break;
+                case '/':div(up1,down1,up2,down2);break;
+            }
         }
-        else if (slash != '/') {
-            error = 1;
-            printf("Invalid-separate numerator and denominator");
-            printf(" by a slash (/)\n");
-        } else if (*denominator <= 0) {
-            error = 1;
-            printf("Invalid—denominator must be positive\n");
-        }
-        /* Discard extra input characters */
-        do {
-            scanf("%c", &discard);
-        } while (discard != '\n');
-    } while (error);
+    }
+    return 0;
 }
--- temp.c
+++ temp.c
@@ -1,17 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-int eval(int n){
-if (n==1) return 1;
-else if (n==0) return 0;
-else {
-    n=n-2;
-    eval(n)
+#include<stdio.h>           /*引用头文件*/
+int gys(int x,int y) {
+    return y?gys(y,x%y):x;  /*递归调用gys，返回最大公约数*/
 }
 
+int gbs(int x,int y) {
+    return x/gys(x,y)*y;
+}
+
+void yuefen(int fz,int fm) {
+    int s=gys(fz,fm);
+    fz/=s;
+    fm/=s;
+    printf("the result is %d/%d\n",fz,fm);
+}
+
+void add(int a,int b,int c,int d) {
+    int u1,u2,v=gbs(b,d),fz1,fm1;
+    u1=v/b*a;
+    u2=v/d*c;
+    fz1=u1+u2;
+    fm1=v;
+    yuefen(fz1,fm1);
+}
+
+void mul(int a,int b,int c,int d) {
+    int u1,u2;
+    u1=a*c;
+    u2=b*d;
+    yuefen(u1,u2);
+}
+
+void sub(int a,int b,int c,int d){
+    int u1,u2,v=gbs(b,d),fz1,fm1;
+    u1=v/b*a;
+    u2=v/d*c;
+    fz1=u1-u2;
+    fm1=v;
+    yuefen(fz1,fm1);
+}
+
+void div(int a,int b,int c,int d){
+    int u1,u2;
+    u1=a*d;
+    u2=b*c;
+    yuefen(u1,u2);
+}
 int main(void){
-    int n=3;
-    printf("%d", eval(n))       
+    char op;
+    char a1,a2;
+    int a,b,c,d;
+    printf("输入两个分数的运算：\n");
+    scanf("%ld%c%ld%c%ld%c%ld",&a,&a1,&b,&op,&c,&a2,&d);
+    if(a1=='/'&&a2=='/'){
+        switch(op){
+            case '+':add(a,b,c,d);break;    /*调用加法函数*/
+            case '*':mul(a,b,c,d);break;    /*调用乘法函数*/
+            case '-':sub(a,b,c,d);break;    /*调用减法函数*/
+            case '/':div(a,b,c,d);break;    /*调用除法函数*/
+        }
+    }
+    else printf("输入的格式不对\n");
     return 0;
-} 
+}
