0ab725ea5a5727b75da6e58f4d4c7f7d431cbf12
Auphie <auphie@gmail.com>
2021-05-02 21:45:52 +0800

    weekly practice

--- temp.c
+++ temp.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX 3
-#define LENGTH 10
 
-void swap(char *str1, char *str2) {
-   char *temp = str1;
-   str1 = str2;
-   str2 = temp;
+void numCmp(char *s1, char *s2) {
+   int r = strcmp(s1,s2);
+   printf("result=%d\n", r); //= -1 (or not 0) if not the same
+   r = strncmp(s1,s2,4);
+   printf("result=%d\n", r); //= 0 because both are fully the same
 }
 
-int main() {
-   char *str1 = "geeks";
-   char *str2 = "forgeeks";
-   swap(str1, str2);
-   printf("str1 is %s, str2 is %s", str1, str2);
-   return 0;
+int myNumCmp(char *s1, char *s2) {
+    if (strlen(s1)>strlen(s2))
+        return 1;
+    else if (strlen(s1)<strlen(s2))
+        return -1;
+    else {
+        for (;(*s1!='\0')&&(*s2!='\0');s1++, s2++) {
+            if (*s1>*s2) return 1;
+            else if (*s1<*s2) return -1;
+        }
+    }
+    return 0;
+}
+
+void split(char* str, char* delim) {
+    char* pch;
+    pch = strtok(str,delim);
+    while (pch != NULL) {
+        printf("%s-",pch);
+        pch = strtok (NULL, delim);
+    }
+}
+
+int main(void){
+    char str1[100]="Can you can a can as a canner can can a can";
+    char str2[10]="can";
+    char str3[10]="ban";
+    char final[20][20];
+//    char str4[]="00:22:33:4B:55:5A";
+    const char *delim = " ";
+//    split(str1, delim);
+	printf("found = %d\n", strstr(str1, str2));
+/*
+    char* strcat1=strcat(str1, str2);
+    printf("strcat1=%s\n", strcat1);
+    char* strcat2=strncat(str1, str2, sizeof(str1)-strlen(str1)-1);
+    strcat2[sizeof(strcat2)]='\0';
+    printf("strcat2=%s\n", strcat2);
+*/
+    return 0;
 }
--- test1.c
+++ test1.c
@@ -1,61 +1,56 @@
-#include<stdio.h>
-#include<string.h>
-#define SIZE 20
-
-void plot(int width, int step, int x_init, int y_init){
-	int x, y;
-	x=x_init+(step+1)/2;
-	if (y%2==0)
-        y=y_init+1;
-    else
-        y=y_init+0;
-	printf("width=%d, x=%d, y=%d \n", width, x, y);
-}
-
-void plot2(int width, int x_init, int y_init){
-	int i, j, x, y;
-	for (i=0; i< width; i++){
-		for (j=0; j< width; j++){
-			y=y_init+i; x=x_init+j;
-			printf("width=%d, init_x=%d, init_y=%d, x=%d, y=%d \n", width, x_init,y_init, y);
-		}
-	}
-}
-
-void square(char data[SIZE], int width, int* i, int* order){
-    int step=0, len=strlen(data), x_init=0, y_init=0;
-    while((*i)<len){
-        step++;
-        *order=step;
-        if(data[*i]=='2'){
-            (*i)++;
-            square(data, width/2, i, order);
-        }
-        else if(data[*i]=='1'){
-            x_init=((*order-1)/2);
-			y_init=((*order-1)/2);
-			printf("0_width=%d, order=%d, step=%d, x_init=%d, y_init=%d \n", width, *order, step, x_init, y_init);
-            (*i)++;
-			if (width == 1)
-				plot(width, step, x_init, y_init);
-			else
-				plot2(width, y_init, x_init);
-        }
-        else if(data[*i]=='0'){
-            (*i)++;
-        }
-        if(step==4) break;
-    }
-}
+#include <stdio.h>
+#include <string.h>
 
 int main(void){
-    int len, i=0, width, order=1;
-    char data[SIZE]="202001010";
-	len=strlen(data);
-    width=4;
-	if (len==1)
-		plot2(4,0,0);
-	else
-	    square(data, width, &i, &order);
+    int count = 0, c = 0, i, j = 0, k, space = 0;
+    char str[100]="Can you can a can as a canner can can a can";
+    char p[50][100], str1[20], ptr1[50][100]; 
+    char *ptr;
+ 
+//    printf("Enter the string\n");
+//    scanf(" %[^\n]s", str);
+    for (i = 0;i<strlen(str);i++){
+        if ((str[i] == ' ')||(str[i] == ',' && str[i+1] == ' ')||(str[i] == '.')){
+            space++;
+        }
+    }
+
+    for (i = 0, j = 0, k = 0;j < strlen(str);j++){
+        if ((str[j] == ' ')||(str[j] == 44)||(str[j] == 46))  {    
+            p[i][k] = '\0';
+            i++;
+            k = 0;
+        }        
+        else
+             p[i][k++] = str[j];
+    }
+
+    k = 0;
+ 
+    for (i = 0;i <= space;i++){
+        for (j = 0;j <= space;j++){
+            if (i == j){
+                strcpy(ptr1[k], p[i]);
+                k++;
+                count++;
+                break;
+            }
+            else{
+                if (strcmp(ptr1[j], p[i]) != 0)
+                    continue;
+                else
+                    break;
+            }
+        }
+    }
+ 
+    for (i = 0;i < count;i++) {
+        for (j = 0;j <= space;j++){
+            if (strcmp(ptr1[i], p[j]) == 0)
+                c++;
+        }
+        printf("%s -> %d times\n", ptr1[i], c);
+        c = 0;
+    }
     return 0;
 }
