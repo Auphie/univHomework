a98adb9dba61b884d5cba1670d29bfcb47a764fb
Auphie <auphie@gmail.com>
2021-05-17 20:27:34 +0800

    weekly practice

--- temp.c
+++ temp.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-void check (char *a, char *b, int (*p)(const char*, const char*)) {
-    if (!(*p) (a,b)) printf("equal");
-    else printf("not equal");
+
+typedef struct cat {
+    char name[10];
+    void (*talk)(int money);
+} cat_t;
+
+void talkLarge(int money) {
+    printf("LARGE %d\n", money);
 }
-int main(void) {
-    char s1[80], s2[80];
-    int (*p)(const char*, const char*)=strcmp;
-    gets(s1);
-    gets(s2);
-    check(s1, s2, p);
+void talkSmall(int money) {
+    printf("SMALL %d\n", money);
+}
+void CatTalk(cat_t catX, int money) {
+    catX.talk(money);
+}
+void test01() {
+    cat_t cat001;
+    strcpy(cat001.name, "Tom");
+    cat001.talk = talkLarge;
+    CatTalk(cat001, 1000);
+    cat001.talk = talkSmall;
+    CatTalk(cat001, 10);
+}
+
+void talkEnglish(char name[]) {
+    printf("Hello! %s, ", name);
+}
+void talkDeutsch(char name[]) {
+    printf("Hallo! %s, ", name);
+}
+
+void run02(void talk(char *), char name[]) {
+    void (*f)(char *);
+    talk(name);
+    talk = talkEnglish;
+    talk(name);
+    f = talk;
+    f(name);
+}
+
+int main() {
+    run02(talkDeutsch, "John");
+    return 0;
 }
